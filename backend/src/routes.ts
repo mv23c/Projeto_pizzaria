3c821223e3efba001fa5a8553261cb88fdc7d681
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-17 23:40:41 -0300

    Adição do serviço de listar categorias.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,12 +4,12 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
 
 const router = Router()
 
 // -- Rotas User --
 router.post('/users', new CreateUserController().handle)
-// Instanciação do objeto do tipo CreateUserController para chamar o método handle.
 
 router.post('/session', new AuthUserController().handle)
 
@@ -17,4 +17,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 // -- Rotas Category --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
 export { router }
