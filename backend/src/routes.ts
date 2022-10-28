25c1cda7c3d3ee547d2e9fb5750021a845589def
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-28 17:18:21 -0300

    Adicionando itens ao pedido

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -11,6 +11,7 @@ import uploadConfig from './config/multer'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 import { CreateOrderController } from './controllers/order/CreateOrderController'
 import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+import { AddItemController } from './controllers/order/AddItemController'
 
 const router = Router()
 
@@ -38,5 +39,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 export { router }
