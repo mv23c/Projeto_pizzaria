ed8f340c1ca25f03bf03e0551e8e5db276259614
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-27 22:13:54 -0300

    Criação de uma order (pedido)

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -9,6 +9,7 @@ import { ListCategoryController } from './controllers/category/ListCategoryContr
 import { CreateProductController } from './controllers/product/CreateProductController'
 import uploadConfig from './config/multer'
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
+import { CreateOrderController } from './controllers/order/CreateOrderController'
 
 const router = Router()
 
@@ -32,4 +33,6 @@ router.post('/product', isAuthenticated, upload.single('file'), new CreateProduc
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
+// -- Rotas Order --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
 export { router }
