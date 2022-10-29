42da38ae28723b64ce69a7c62290a95c104c687b
mv23c <100789090+mv23c@users.noreply.github.com>
2022-10-29 10:28:50 -0300

    Instalação do sass

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,8 @@
+import styles from '../../styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Sujeito Pizza :)</h1>
     </div>
   )
