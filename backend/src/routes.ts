284353f4c50968d5b9981d289eb32f3e3fc3b829
mv23c <100789090+mv23c@users.noreply.github.com>
2022-11-06 20:54:03 -0300

    Criado a funcionalidade de finalizar uma order. Finalização do backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -16,6 +16,7 @@ import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/SendOrderController'
 import { ListOrdersController } from './controllers/order/ListOrdersController'
 import { DetailOrderController } from './controllers/order/DetailOrderController'
+import { FinishOrderController } from './controllers/order/FinishOrderController'
 
 const router = Router()
 
@@ -52,6 +53,8 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
 
-router.get('/order/detail', isAuthenticated, new DetailOrderController().handle )
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle)
 
 export { router }
