40fdea93915e7729845b3932699f5e636874b794
mv23c <100789090+mv23c@users.noreply.github.com>
2022-11-14 13:51:27 -0300

    Implementação de um context

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext, FormEvent } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
@@ -5,8 +6,22 @@ import logoImg from '../../public/logo.svg';
 import { Input } from '../components/ui/Input';
 import { Button } from '../components/ui/Button';
 import Link from 'next/link';
+import { AuthContext } from '../contexts/AuthContext';
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: "teste@email.com",
+      password: "123456"
+    }
+
+    await signIn(data);
+  }
+
   return (
     <>
     <Head>
@@ -17,7 +32,7 @@ export default function Home() {
       <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
     
       <div className={styles.login}>
-        <form>
+        <form onSubmit={handleLogin}>
           <Input
             placeholder="Digite seu email" 
             type="text"
