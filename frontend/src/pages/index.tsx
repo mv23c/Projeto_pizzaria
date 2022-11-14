c208b48e0bd067c76bca22b4412ad42223fce5b8
mv23c <100789090+mv23c@users.noreply.github.com>
2022-11-14 14:08:20 -0300

    Preparando a página de login para receber os dados digitados pelo usuário

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, FormEvent } from 'react';
+import { useContext, FormEvent, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss';
@@ -10,13 +10,16 @@ import { AuthContext } from '../contexts/AuthContext';
 
 export default function Home() {
   const { signIn } = useContext(AuthContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
 
     let data = {
-      email: "teste@email.com",
-      password: "123456"
+      email,
+      password
     }
 
     await signIn(data);
@@ -36,11 +39,15 @@ export default function Home() {
           <Input
             placeholder="Digite seu email" 
             type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <Input
             placeholder="Sua senha" 
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <Button 
