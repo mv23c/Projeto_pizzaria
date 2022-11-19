448e4409b90b7dd2f4ef0343cd62bd4d832eeaff
mv23c <100789090+mv23c@users.noreply.github.com>
2022-11-18 22:44:39 -0300

    Configurando a função de loading e preparação para cadastro

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -17,12 +17,21 @@ export default function Home() {
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
 
+    if(email === '' || password === '') {
+      alert("Preencha os dados!");
+      return;
+    }
+
+    setLoading(true);
+
     let data = {
       email,
       password
     }
 
     await signIn(data);
+
+    setLoading(false);
   }
 
   return (
@@ -52,7 +61,7 @@ export default function Home() {
 
           <Button 
             type="submit"
-            loading={false}
+            loading={loading}
           >
             Acessar
           </Button>
--- frontend/src/pages/signup/index.tsx
+++ frontend/src/pages/signup/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../../styles/home.module.scss';
@@ -7,6 +8,22 @@ import { Button } from '../../components/ui/Button';
 import Link from 'next/link';
 
 export default function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+
+    if(name === '' || email === '' || password === '') {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    setLoading(true);
+  }
+
   return (
     <>
     <Head>
@@ -18,25 +35,31 @@ export default function SignUp() {
     
       <div className={styles.login}>
         <h1>Criando sua conta</h1>
-        <form>
+        <form onSubmit={handleSignUp}>
           <Input
             placeholder="Digite seu nome" 
             type="text"
+            value={name}
+            onChange = { (e) => setName(e.target.value) }
           />
 
           <Input
             placeholder="Digite seu email" 
             type="text"
+            value={email}
+            onChange = { (e) => setEmail(e.target.value) }
           />
 
           <Input
             placeholder="Sua senha" 
             type="password"
+            value={password}
+            onChange = { (e) => setPassword(e.target.value) }
           />
 
           <Button 
             type="submit"
-            loading={false}
+            loading={loading}
           >
             Cadastrar
           </Button>
