aea4acd9139f23baec8a44a3c6a4c8e9f0c650f3
mv23c <100789090+mv23c@users.noreply.github.com>
2022-12-13 10:16:36 -0300

    Enviando a imagem do produto

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -1,10 +1,33 @@
 
+import { useState, ChangeEvent } from "react";
 import Head from "next/head";
 import styles from './styles.module.scss';
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Header } from "../../components/Header";
+import { FiUpload } from 'react-icons/fi';
+import Image from "next/image";
 
 export default function Product(){
+
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState(null);
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>){
+        if(!e.target.files){
+            return;
+        }
+        const image = e.target.files[0];
+
+        if(!image){
+            return;
+        }
+
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
     return(
         <>
             <Head>
@@ -18,6 +41,25 @@ export default function Product(){
                 <h1>Novo produto</h1>
 
                 <form className={styles.form}>
+
+                    <label className={styles.labelAvatar}>
+                        <span>
+                            <FiUpload size={25} color="#FFF" />
+                        </span>
+
+                        <input type="file" accept="image/png, image/jpeg" onChange={handleFile} />
+
+                        {avatarUrl && (
+                          <Image 
+                            className={styles.preview}
+                            src={avatarUrl}
+                            alt="Foto do produto"
+                            width={250}
+                            height={250}
+                          />
+                        )}
+
+                    </label>
 
                     <select>
                         <option>
