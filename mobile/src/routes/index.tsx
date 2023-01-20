617e3f764d1dccd2821c1a4ee6d8beac9174003e
mv23c <100789090+mv23c@users.noreply.github.com>
2023-01-20 19:19:11 -0300

    Criação do loading e da tela de login estática

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,11 +1,28 @@
 import React from "react";
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
 function Routes(){
     const isAuthenticated = false;
     const loading = false;
+
+    if(loading){
+        return(
+            <View 
+              style={{
+                flex: 1, 
+                backgroundColor: '#1D1D2E', 
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <ActivityIndicator size={60} color='#F5f7fb' />
+            </View>
+        )
+    }
+
+
     return(
         isAuthenticated ? <AppRoutes/> : <AuthRoutes/>
     )
