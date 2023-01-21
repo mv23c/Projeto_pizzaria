2bdf6a95dac6253edfdd361246a9744f6581adff
mv23c <100789090+mv23c@users.noreply.github.com>
2023-01-21 15:52:03 -0300

    Criação do contexto e preparação para requisições

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -11,18 +11,16 @@ import { AuthContext } from '../../contexts/AuthContext';
 
 
 export default function SignIn(){
-    const { user } = useContext(AuthContext);
-
-
+    const { signIn } = useContext(AuthContext);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    function handleLogin(){
+    async function handleLogin(){
         if(email === '' || password === ''){
             return;
         }
 
-        alert('Email digitado: ' + email);
+        await signIn({ email, password })
     }
 
     return(
